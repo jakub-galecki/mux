e2c44b23fe0334466f4e8dc30ae04bc960ce5557
jac30b <galeckijakub9@gmail.com>
2021-11-01 15:59:51 +0100

    Fix checking params

--- router.go
+++ router.go
@@ -19,10 +19,27 @@ func (_router *Router) RegisterRoute(method string, pattern string,
 
 func (_router *Router) match(method string, url *url.URL) (*route, *Error) {
 	pattern, expectedParams := parseURL(url)
+	checkParams := func(arr1 []reflect.Value, arr2 []string) bool {
+		if len(arr1) == 0 && len(arr2) == 0 {
+			return true
+		}
+		if len(arr1) != len(arr2) {
+			return false
+		}
+		exists := make(map[string]bool)
+		for _, v := range arr1 {
+			exists[v.Interface().(string)] = true
+		}
+		for _, v := range arr2 {
+			if !exists[v] {
+				return false
+			}
+		}
+		return true
+	}
 	for _, _route := range _router.routes {
 		if strings.EqualFold(method, _route.method) && strings.EqualFold(pattern, _route.pattern) &&
-			(len(reflect.ValueOf(expectedParams).MapKeys()) == 0 && len(_route.expectedParams) == 0) ||
-			reflect.DeepEqual(reflect.ValueOf(expectedParams).MapKeys(), _route.expectedParams) {
+			(checkParams(reflect.ValueOf(expectedParams).MapKeys(), _route.expectedParams)) {
 			return _route, nil
 		}
 	}
